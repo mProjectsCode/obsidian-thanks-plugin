8142d8a8e2b505140c72eade089a696d583db801
Moritz Jung <m.projects.code@gmail.com>
2024-09-30 21:27:45 +0200

    no guarantees that this works

--- src/modal.ts
+++ src/modal.ts
@@ -1,6 +1,6 @@
-import { ButtonComponent, Modal } from 'obsidian';
+import { arrayBufferToBase64, ButtonComponent, Modal, requestUrl, RequestUrlParam, Setting } from 'obsidian';
 import ThanksPlugin from './main';
-import { Device, AuthReply } from './types';
+import { Device, AuthReply, Token, RepoData, PluginRepoData, ThemeRepoData } from './types';
 import { post, api_options } from './authenticate';
 
 export class AuthModal extends Modal {
@@ -17,16 +17,6 @@ export class AuthModal extends Modal {
 		this.token = undefined;
 		this.pollInterval = undefined;
 	}
-
-	// /**
-	//  * Displayed immediately as placeholder.
-	//  * This placeholder has as button, which when clicked tries to do authentication.
-	//  * The reason this is a seperate user action is so we can be _extra clear_ that it requires internet acces.
-	//  */
-	// displayPlaceholderContent(): void {
-	//     this.contentEl.empty();
-	//     this.contentEl.createEl('h2', { text: 'Click the button?' });
-	// }
 
 	/**
 	 * Display authentication steps, so we can grab info from github.
@@ -48,28 +38,144 @@ export class AuthModal extends Modal {
 	displayAuthWaitContent(device: Device): void {
 		this.contentEl.empty();
 		this.contentEl.createEl('h2', { text: 'Please sign in with GitHub' });
-
+		// TODO: Make it so you can easily copy paste the link and code
 		this.contentEl.createEl('p', { text: `Go to ${device.verification_uri} and enter code ${device.user_code}` });
 
 		this.contentEl.createEl('p', { text: `Waiting for auth response...` });
 	}
 
 	async displayStarContent(): Promise<void> {
+		const repoData = await this.getRepoData();
+
 		this.contentEl.empty();
 		this.contentEl.createEl('h2', { text: 'Thank you, plugins & themes!' });
+
+		// TODO: show them in the modal in a nice way
+		for (const repo of repoData) {
+			// this.contentEl.createEl('p', { text: `${repo.name} - ${repo.is_starred}` });
+			const setting = new Setting(this.contentEl);
+			setting.setName(repo.name);
+			setting.setDesc(`Join ${repo.star_count} others in starring this repo!`);
+			if (!repo.is_starred) {
+				setting.addButton(button => {
+					button.setButtonText('Star');
+					button.setCta();
+					button.onClick(async () => {
+						// TODO: star the repo
+						console.log('Star', repo.repo);
+					});
+				});
+			} else {
+				setting.addButton(button => {
+					button.setButtonText('Star');
+					button.setDisabled(true);
+					button.setTooltip('Already starred');
+				});
+			}
+		}
+	}
+
+	async getStarCount(repo: string): Promise<number> {
+		return -1;
+	}
+
+	/**
+	 * Gets all the data from github repos we want.
+	 * TODO: figure out the types in a nice way
+	 */
+	async getRepoData(): Promise<RepoData[]> {
+		const result: RepoData[] = [];
 
 		// get list of plugins and themes
 		const pluginList = await this.plugin.getPluginData();
 		const themeList = await this.plugin.getThemeData();
 
-		// show them in the modal in a nice way
+		// get list of already starred repositories
+		const alreadyStarred = await this.getStarredRepositories();
+
+		// get list of repos to star
+		// const reposToStar: Record<string, string[]> = {};
+
 		for (const plugin of pluginList) {
-			this.contentEl.createEl('p', { text: plugin.name });
+			const isStarred = alreadyStarred.has(plugin.repo);
+			result.push({
+				id: plugin.id,
+				name: plugin.name,
+				repo: plugin.repo,
+				star_count: isStarred ? alreadyStarred.get(plugin.repo)!.star_count : await this.getStarCount(plugin.repo),
+				is_starred: isStarred,
+			} satisfies PluginRepoData);
 		}
 
 		for (const theme of themeList) {
-			this.contentEl.createEl('p', { text: theme.name });
+			const isStarred = alreadyStarred.has(theme.repo);
+			result.push({
+				name: theme.name,
+				repo: theme.repo,
+				star_count: isStarred ? alreadyStarred.get(theme.repo)!.star_count : await this.getStarCount(theme.repo),
+				is_starred: isStarred,
+			} satisfies ThemeRepoData);
 		}
+
+		return result;
+	}
+
+	// TODO: implement this method.
+	// See: https://docs.github.com/en/rest/activity/starring?apiVersion=2022-11-28#list-repositories-starred-by-the-authenticated-user
+	// Map<repo, RepoData>
+	async getStarredRepositories(): Promise<Map<string, RepoData>> {
+		if (!this.token) {
+			// not authenticated, do nothing
+			throw new Error('Not authenticated');
+		}
+		// Assume we have a token.
+		let lastPage = 1;
+		const response = await requestUrl({
+			url: `https://api.github.com/user/starred?per_page=100?page=${lastPage}`,
+			method: 'GET',
+			headers: {
+				Accept: 'application/vnd.github+json',
+				Authentication: `Bearer ${this.token}`,
+				'X-GitHub-Api-Version': '2022-11-28',
+			},
+			throw: false,
+		});
+
+		console.log(response.headers);
+
+		// TODO: response.headers will contain a link header with rel="next" attached to it, as well as rel="last"
+		// TODO: set pageNumber to last
+
+		let promisePool = [];
+		for (let i = 2; i < lastPage; i++) {
+			let req = requestUrl({
+				url: `https://api.github.com/user/starred?per_page=100?page=${i}`,
+				method: 'GET',
+				headers: {
+					Accept: 'application/vnd.github+json',
+					Authentication: `Bearer ${this.token}`,
+					'X-GitHub-Api-Version': '2022-11-28',
+				},
+				throw: false,
+			});
+			promisePool.push(req);
+		}
+
+		// let responses = [response, ...await Promise.all(promisePool)].flat();
+		// let responseJSON = responses.map(r => r.json);
+
+		let starred_repos: Map<string, RepoData> = new Map();
+		// for (const obj in responses) {
+		//     const starredResult = {
+		// 		name: "unused",
+		//         repo: obj.full_name,
+		//         star_count: obj.stargazers_count,
+		//         is_starred: true
+		//     } satisfies RepoData
+		//     starred_repos.set(starredResult.repo, starredResult);
+		// }
+
+		return starred_repos;
 	}
 
 	async onOpen(): Promise<void> {
@@ -103,7 +209,7 @@ export class AuthModal extends Modal {
 			grant_type: 'urn:ietf:params:oauth:grant-type:device_code',
 		};
 
-		let interval = device_object.interval;
+		let interval = device_object.interval + 5;
 
 		let time_remaining = device_object.expires_in;
 
@@ -116,52 +222,69 @@ export class AuthModal extends Modal {
 			const reply = await post(authorize_url, authorize_options);
 
 			console.log(reply);
-			const data = JSON.parse(reply) as AuthReply;
+			const data = JSON.parse(reply) as AuthReply | Token;
+			// const data = JSON.parse(reply) as AuthReply;
 
-			if (data.error) {
+			if ('error' in data) {
 				if (data.error === 'authorization_pending') {
 					console.log('Authorization pending...');
 					return;
 				} else if (data.error === 'slow_down') {
-					interval = data.interval ?? interval;
+					interval = data.interval ? data.interval + 5 : interval;
 					this.pollInterval?.setNewInterval(interval * 1000);
 					console.log('Slow down...');
 					return;
 				} else {
 					console.log('Error: ', data.error_description);
+					this.pollInterval?.stop();
+					// TODO: display error message
 					return;
 				}
 			} else {
 				console.log('Success: ', data);
 				this.pollInterval?.stop();
+				this.token = data.access_token;
+				await this.displayStarContent();
 			}
 		});
 
 		this.pollInterval.start();
-
-		this.token = '...';
 	}
 }
 
 class CustomInterval {
 	interval: number;
 	cb: () => Promise<void>;
+	stopped: boolean;
 
 	_timeout: number | undefined;
 
 	constructor(interval: number, cb: () => Promise<void>) {
 		this.interval = interval;
 		this.cb = cb;
+		this.stopped = false;
 	}
 
 	start(): void {
+		this.stopped = false;
+		this._start();
+	}
+
+	_start(): void {
+		if (this.stopped) {
+			return;
+		}
+
+		console.log('starting interval', this.interval);
+
 		this._timeout = window.setTimeout(async () => {
 			await this.cb();
-			this.start();
+			this._start();
 		}, this.interval);
 	}
 
 	stop(): void {
+		this.stopped = true;
 		if (this._timeout !== undefined) {
 			window.clearTimeout(this._timeout);
 		}
--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,7 @@
-// Plugins:
-// https://raw.githubusercontent.com/obsidianmd/obsidian-releases/master/community-plugins.json
-
+/**
+ * The raw plugin data as we obtain from parsing
+ * https://raw.githubusercontent.com/obsidianmd/obsidian-releases/master/community-plugins.json
+ */
 export interface RawPluginData {
 	id: string;
 	name: string;
@@ -9,14 +10,19 @@ export interface RawPluginData {
 	repo: string;
 }
 
+/**
+ * The plugin data that we care about: its name, id, and link to repository.
+ */
 export interface PluginData {
 	name: string;
 	id: string;
 	repo: string;
 }
 
-// CSS Themes:
-// https://raw.githubusercontent.com/obsidianmd/obsidian-releases/master/community-css-themes.json
+/**
+ * The raw theme data as we obtain from parsing
+ * https://raw.githubusercontent.com/obsidianmd/obsidian-releases/master/community-css-themes.json
+ */
 export interface RawThemeData {
 	name: string;
 	author: string;
@@ -25,30 +31,97 @@ export interface RawThemeData {
 	modes: string[];
 }
 
+/**
+ * The theme data we care about: its name, and link to repository.
+ */
 export interface ThemeData {
 	name: string;
 	repo: string;
 }
 
 /**
- Parameter name	Type	Description
- device_code	string	The device verification code is 40 characters and used to verify the device.
- user_code	string	The user verification code is displayed on the device so the user can enter the code in a browser. This code is 8 characters with a hyphen in the middle.
- verification_uri	string	The verification URL where users need to enter the user_code: https://github.com/login/device.
- expires_in	integer	The number of seconds before the device_code and user_code expire. The default is 900 seconds or 15 minutes.
- interval	integer	The minimum number of seconds that must pass before you can make a new access token request (POST https://github.com/login/oauth/access_token) to complete the device authorization. For example, if the interval is 5, then you cannot make a new request until 5 seconds pass. If you make more than one request over 5 seconds, then you will hit the rate limit and receive a slow_down error.
+ * A 'Device' response used for communicating with the github API.
  */
 export interface Device {
+	/**
+	 * The device verification code is 40 characters and used to verify the device.
+	 */
 	device_code: string;
+	/**
+	 * The user verification code is displayed on the device so the user can enter the code in a browser. This code is 8 characters with a hyphen in the middle.
+	 */
 	user_code: string;
+	/**
+	 * The verification URL where users need to enter the user_code: https://github.com/login/device.
+	 */
 	verification_uri: string;
+	/**
+	 * The number of seconds before the device_code and user_code expire. The default is 900 seconds or 15 minutes.
+	 */
 	expires_in: number;
+	/**
+	 * The minimum number of seconds that must pass before you can make a new access token request (POST https://github.com/login/oauth/access_token) to complete the device authorization. For example, if the interval is 5, then you cannot make a new request until 5 seconds pass. If you make more than one request over 5 seconds, then you will hit the rate limit and receive a slow_down error.
+	 */
 	interval: number;
 }
 
+/**
+ * An 'Auth' response used for communicating with the github API.
+ */
 export interface AuthReply {
+	/**
+	 * The error identifier.
+	 * See: https://docs.github.com/en/apps/oauth-apps/building-oauth-apps/authorizing-oauth-apps#error-codes-for-the-device-flow
+	 */
 	error: string;
+	/**
+	 * Textual representation to the error.
+	 */
 	error_description: string;
+	/**
+	 * Link to the docs for this error.
+	 */
 	error_uri: string;
+	/**
+	 * Optional new interval to abide by when making POST requests.
+	 * See Device.interval.
+	 */
 	interval?: number;
 }
+
+/**
+ * A 'Token' response used for communicating with the github API.
+ */
+export interface Token {
+	/**
+	 * The access token to communicate with the API.
+	 */
+	access_token: string;
+	/**
+	 * The scope that this token has.
+	 * For starring plugins, this must be `public_repo`.
+	 */
+	scope: string;
+	/**
+	 * The type of token this is.
+	 * We expect Bearer tokens.
+	 */
+	token_type: string;
+}
+
+export interface PluginRepoData {
+	id: string;
+	name: string;
+	repo: string;
+	star_count: number;
+	is_starred: boolean;
+}
+
+export interface ThemeRepoData {
+	name: string;
+	repo: string;
+	star_count: number;
+	is_starred: boolean;
+}
+
+export type RepoData = PluginRepoData | ThemeRepoData;
